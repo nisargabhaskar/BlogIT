f1ad0766a87439b316312b370b4e8c80b08c110e
nisargabhaskar <nisarga.bhaskar02@gmail.com>
2023-04-23 08:51:07 +0530

    all pods are running

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ kubectl delete services --all
 kubectl delete deployments --all
 kubectl delete pods --all
 docker image rm blogit
-docker build -f flask-app-image.dockerfile . -t blogit
+
 kubectl apply -f secret.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f deployments.yaml
